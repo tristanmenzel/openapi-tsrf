4f5663449882274a8237c5914b6a06337e532636
Tristan Menzel <tristanmenzel@gmail.com>
2023-04-05 15:31:43 -0700

    Replace [ and ] characters in type and operation names

--- src/cli/sanitization.ts
+++ src/cli/sanitization.ts
@@ -1,7 +1,7 @@
 import { camelCase } from 'change-case'
 
 const replaceInvalidWithUnderscore = (value: string) =>
-  value.replace(/[.\\\/ \-]+/g, '_')
+  value.replace(/[.\\\/ \-[\]]+/g, '_')
 
 export const makeSafeTypeIdentifier = replaceInvalidWithUnderscore
 export const makeSafeMethodIdentifier = (value: string) =>
